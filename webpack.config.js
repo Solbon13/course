818815fc759039223d5d946660aaef845d90db7d
Solbon <Solbon13@yandex.ru>
2019-12-25 08:59:46 +0800

    work commit 3

--- webpack.config.js
+++ webpack.config.js
@@ -41,5 +41,9 @@ module.exports = {
             filename: "[name].css",
             chunkFilename: "[id].css"
         })
-    ]
+    ],
+    devServer: {  // configuration for webpack-dev-server
+        contentBase: './src/public',  //source of static assets
+        port: 7700, // port to run dev-server
+    }
 };
